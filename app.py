939b4757dc2c4ceaee6d6772af4db0eec9af98b7
felip <soitfelip6@gmail.com>
2026-02-10 21:21:45 +0700

    Update: change name

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.caption("Analisis dan visualisasi hasil kuesioner responden")
 # =============================
 # LOAD DATA
 # =============================
-df = pd.read_excel("data_kuesioner.xlsx")
+df = pd.read_csv("data_kuesioner.csv")
 
 
 # Ambil kolom pertanyaan (selain ID/responden)

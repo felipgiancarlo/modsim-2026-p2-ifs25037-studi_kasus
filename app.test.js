6a04be5b48b0922735b77fc437c609c2ebe3bd93
felip <soitfelip6@gmail.com>
2026-03-01 10:18:42 +0700

    Delete app.test.js

--- app.test.js
+++
@@ -1,9 +0,0 @@
-const { callAxios } = require("./_utils");
-const { baseUrl } = require("./_config");
-
-describe("Website Checking", () => {
-  test("valid: website dapat diakses", async () => {
-    const res = await callAxios("get", baseUrl);
-    expect(res.status).toBe(200);
-  });
-});
